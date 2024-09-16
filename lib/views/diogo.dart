28a943cc69197fa04b883c8b0893c40f255c5f91
diogofragoso <36908574+diogofragoso@users.noreply.github.com>
2024-09-16 16:37:42 -0300

    Criando um arquivo novo para o diogo

---
+++ lib/views/diogo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Diogo extends StatefulWidget {
+  const Diogo({super.key});
+
+  @override
+  State<Diogo> createState() => _DiogoState();
+}
+
+class _DiogoState extends State<Diogo> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('ola');
+  }
+}
