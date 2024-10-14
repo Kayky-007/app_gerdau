819e9c003d2c016914fe22b3544f0578a8c863c1
Cauacomtilnoa <csj_sres@hotmail.com>
2024-10-14 14:48:12 -0300

    Estilizando Navigation Bar
    - Troquei as cores de fundo e dos icones da navigation bar
    
    - Como essa parte foi feita com o Widget NavigationBar e não com o bottomNavigationBar, tive que envelopa-lá com o NavigationBarTheme para conseguir alterar a cor do texto

--- lib/view/components/barra_navigacao.dart
+++ lib/view/components/barra_navigacao.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD:lib/views/components/barra_navigacao.dart
-import 'package:login_gerdau/views/cardapio_view.dart';
-import 'package:login_gerdau/views/inicio_view.dart';
-import 'package:login_gerdau/views/perfil_view.dart';
-=======
+import 'package:login_gerdau/view/cardapio_view.dart';
 import 'package:login_gerdau/view/inicio_view.dart';
 import 'package:login_gerdau/view/perfil_view.dart';
->>>>>>> 64ab53cb03f136cca7a9a7d267c2ba293bed69bd:lib/view/components/barra_navigacao.dart
 
 class BarraNavigacao extends StatefulWidget {
   const BarraNavigacao({super.key});
@@ -35,30 +30,57 @@ class _BarraNavigacaoState extends State<BarraNavigacao> {
           ),
         ],
       ),
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        indicatorColor: Color.fromRGBO(255, 204, 8, 1),
-        selectedIndex: currentPageIndex,
-        destinations: const <NavigationDestination>[
-          NavigationDestination(
-            icon: Icon(Icons.home_outlined),
-            label: 'Inicio',
-            selectedIcon: Icon(Icons.home),
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.fastfood_outlined),
-            label: 'Cardápio',
-            selectedIcon: Icon(Icons.fastfood_rounded),
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.person),
-            label: 'Perfil',
-          ),
-        ],
+      bottomNavigationBar: NavigationBarTheme( // 'Envelopa' o Navigation Bar, foi usado pra trocar a cor do texto
+        data: NavigationBarThemeData(
+          labelTextStyle: WidgetStatePropertyAll(  
+            TextStyle(color: Colors.white)  // Deixa qualquer texto da navigation bar com a cor branca
+          )
+        ),
+        child: NavigationBar(  // Navigation Bar, ATENÇÃO, ta sendo usado o Navigation Bar aqui, não o BottomNavigationBar
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+          backgroundColor: Color.fromARGB(255, 1, 52, 96),
+          indicatorColor: Color.fromRGBO(0, 74, 143, 1),
+          selectedIndex: currentPageIndex,
+          destinations: const <NavigationDestination>[
+            NavigationDestination(
+              icon: Icon(
+                Icons.home_outlined,
+                color: Colors.white,
+                ),
+              label: 'Inicio',
+              selectedIcon: Icon(
+                Icons.home,
+                color: Colors.white
+                ),
+            ),
+            NavigationDestination(
+              icon: Icon(
+                Icons.fastfood_outlined,
+                color: Colors.white,
+              ),
+              label: 'Cardápio',
+              selectedIcon: Icon(
+                Icons.fastfood_rounded,
+                color: Colors.white,
+              ),
+            ),
+            NavigationDestination(
+              icon: Icon(
+                Icons.person_outline,
+                color: Colors.white,
+                ),
+                selectedIcon: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                  ),
+              label: 'Perfil',
+            ),
+          ],
+        ),
       ),
     );
   }
