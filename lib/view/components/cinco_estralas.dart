c9224b4d4da39a650af181c1ca8c57714cf5bff7
Kayky <149194790+Kayky-007@users.noreply.github.com>
2024-10-15 16:43:21 -0300

    Organização de codigo atualizado

--- lib/view/components/cinco_estralas.dart
+++ lib/view/components/cinco_estralas.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:login_gerdau/view/components/appbar_components.dart';
-import 'package:login_gerdau/view/components/espacamento_h.dart';
 
-class CincoEstrelas extends StatefulWidget {
-  const CincoEstrelas({super.key});
+class CincoEstralas extends StatefulWidget {
+  const CincoEstralas({super.key});
 
   @override
-  State<CincoEstrelas> createState() => _CincoEstrelasState();
+  State<CincoEstralas> createState() => _ClassificacaoComponentState();
 }
 
-class _CincoEstrelasState extends State<CincoEstrelas> {
+class _ClassificacaoComponentState extends State<CincoEstralas> {
   int _cliques = 0;
 
   void _atualizarClassificacao(int novaClassificacao) {
@@ -20,60 +18,27 @@ class _CincoEstrelasState extends State<CincoEstrelas> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: NestedScrollView(
-        headerSliverBuilder: (context, innerBoxScrolled) => [
-          AppbarfotoComponents(title: 'Cinco Estrelas'),
-        ],
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            EspacamentoH(h: 20),
-            Text(
-              'Strogonoff de Frango',
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-            ),
-            EspacamentoH(h: 20),
-            Image.asset(
-              'assets/images/prato_exemplo.jpg',
-              width: 400,
-            ),
-            EspacamentoH(h: 20),
-            const Text(
-              'Avalie o Prato:',
-              style: TextStyle(fontSize: 22),
-            ),
-            EspacamentoH(h: 15),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: List.generate(5, (index) {
-                return IconButton(
-                  icon: Icon(
-                    _cliques > index ? Icons.star : Icons.star_border,
-                    color: Colors.amber,
-                    size: 40,
-                  ),
-                  onPressed: () => _atualizarClassificacao(index + 1),
-                );
-              }),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Avaliação: $_cliques',
-              style: TextStyle(fontSize: 17),
-            ),
-            EspacamentoH(h: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                'Enviar',
-                style: TextStyle(fontSize: 17),
+    return Column(
+      children: [
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: List.generate(5, (index) {
+            return IconButton(
+              icon: Icon(
+                _cliques > index ? Icons.star : Icons.star_border,
+                color: Colors.amber,
+                size: 40,
               ),
-            )
-          ],
+              onPressed: () => _atualizarClassificacao(index + 1),
+            );
+          }),
         ),
-      ),
+        const SizedBox(height: 20),
+        Text(
+          'Avaliação: $_cliques',
+          style: const TextStyle(fontSize: 17),
+        ),
+      ],
     );
   }
 }
-
--- lib/view/inicio_view.dart
+++ lib/view/inicio_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:login_gerdau/view/components/appbar_components.dart';
 import 'package:login_gerdau/view/components/cinco_estralas.dart';
+import 'package:login_gerdau/view/components/espacamento_h.dart';
 
 class InicioView extends StatefulWidget {
   const InicioView({super.key});
@@ -12,9 +14,49 @@ class _InicioViewState extends State<InicioView> {
   @override
   Widget build(BuildContext context) {
 
-    return const Scaffold(
-        body:
-           CincoEstrelas(),
-        );
+    return Scaffold(
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxScrolled) => [
+          AppbarfotoComponents(title: 'Cinco Estrelas'),
+        ],
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              EspacamentoH(h: 20),
+              Text(
+                'Strogonoff de Frango',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              EspacamentoH(h: 20),
+              Image.asset(
+                'assets/images/prato_exemplo.jpg',
+                width: 400,
+              ),
+              EspacamentoH(h: 20),
+              const Text(
+                'Avalie o Prato:',
+                style: TextStyle(fontSize: 22),
+              ),
+              EspacamentoH(h: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CincoEstralas()
+              ],
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'Enviar',
+                  style: TextStyle(fontSize: 17),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
