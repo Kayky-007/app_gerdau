adf1baa94b5058b5554e39e095caa2f5df560a73
Kayky <149194790+Kayky-007@users.noreply.github.com>
2024-10-30 14:09:34 -0300

    Organização do component cinco estrelas e inicio_view

--- lib/view/inicio_view.dart
+++ lib/view/inicio_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:login_gerdau/view/components/appbar_components.dart';
 import 'package:login_gerdau/view/components/cinco_estralas.dart';
+import 'package:login_gerdau/view/components/espacamento_h.dart';
 
 class InicioView extends StatefulWidget {
   const InicioView({super.key});
@@ -11,10 +13,29 @@ class InicioView extends StatefulWidget {
 class _InicioViewState extends State<InicioView> {
   @override
   Widget build(BuildContext context) {
-
-    return const Scaffold(
-        body:
-           CincoEstrelas(),
-        );
+    return Scaffold(
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxScrolled) => [
+          AppbarfotoComponents(title: 'Inicio'),
+        ],
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            EspacamentoH(h: 20),
+            const Text(
+              'Strogonoff de Frango',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            EspacamentoH(h: 20),
+            Image.asset(
+              'assets/images/prato_exemplo.jpg',
+              width: 400,
+            ),
+            EspacamentoH(h: 20),
+             CincoEstrelas(), 
+          ],
+        ),
+      ),
+    );
   }
 }
