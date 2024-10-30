d8555e9a1224e8e0d6edfabd1683efa5d5a56d1c
Victor Salvador <148452822+victorhhugo18@users.noreply.github.com>
2024-10-30 16:45:37 -0300

    Foi adicionado um alert para a confirmação do pedido

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dropdown_alert/dropdown_alert.dart';
 import 'package:login_gerdau/view/cardapio_view.dart';
 
 import 'package:login_gerdau/view/login_view.dart';
@@ -14,6 +15,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, child) => Stack(
+        children: [child!, DropdownAlert()],
+      ),
       //cores do app
       theme: ThemeData(
           scaffoldBackgroundColor: Colors.white,
