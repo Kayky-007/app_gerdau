7dd930bdc71e2929df38f687cd5741878cf37b6c
Guilherme Ribeiro <guilhermeguiribeiro22@gmail.com>
2024-11-07 16:20:39 -0300

    Atualização Login
    -Foi adicionado a função de login
    -Foi listado o nome e e-mail de usuário

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_gerdau/controller/login_controller.dart';
 import 'package:login_gerdau/view/components/espacamento_h.dart';
 
 class LoginView extends StatefulWidget {
@@ -10,6 +11,38 @@ class LoginView extends StatefulWidget {
 
 class _LoginViewState extends State<LoginView> {
   bool _rememberMe = false;
+  bool _isLoading = false; // Para mostrar o indicador de carregamento
+  LoginController _controller = LoginController();
+
+  // Função para tentar o login
+  Future<void> _tentarLogin() async {
+    setState(() {
+      _isLoading = true; // Ativa o carregamento
+    });
+
+    try {
+      bool sucesso = await _controller.logar(); // Chama o login
+
+      if (sucesso) {
+        // Se o login for bem-sucedido, redireciona para a tela inicial
+        Navigator.pushReplacementNamed(context, '/painel');  // Usando pushNamed
+      } else {
+        // Caso o login falhe
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Usuário e/ou senha inválidos.')),
+        );
+      }
+    } catch (e) {
+      // Captura qualquer exceção e exibe uma mensagem genérica de erro
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Usuário e/ou senha inválidos.')),
+      );
+    } finally {
+      setState(() {
+        _isLoading = false; // Desativa o carregamento
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,102 +56,102 @@ class _LoginViewState extends State<LoginView> {
             ),
           ),
         ),
-        backgroundColor:  Color.fromRGBO(9, 68, 121, 1),
+        backgroundColor: Color.fromRGBO(9, 68, 121, 1),
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/logo_fundo3.jpg'),  // Substitua pelo caminho da sua imagem de fundo
-            fit: BoxFit.fill,  // Isso faz a imagem cobrir o fundo
-            opacity: 1,  // Opacidade para suavizar o efeito de fundo (ajuste conforme necessário)
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/logo_fundo3.jpg'),  // Substitua pelo caminho da sua imagem de fundo
+              fit: BoxFit.cover,  // Ajusta para cobrir toda a tela
+            ),
           ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Center(
-            child: SingleChildScrollView(
-              child: Card(
-                color: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12.0),
-                ),
-                elevation: 5,
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'assets/images/logo_gerdau_login.png',
-                        width: 150,
-                        height: 150,
-                      ),
-                      EspacamentoH(h: 10),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Chapa',
-                          prefixIcon:
-                              Icon(Icons.badge, color: Colors.blueGrey[800]),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Center(
+              child: SingleChildScrollView(
+                child: Card(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  elevation: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/logo_gerdau_login.png',
+                          width: 150,
+                          height: 150,
+                        ),
+                        EspacamentoH(h: 10),
+                        TextField(
+                          controller: _controller.email_usuario,
+                          decoration: InputDecoration(
+                            labelText: 'Chapa',
+                            prefixIcon: Icon(Icons.badge, color: Colors.blueGrey[800]),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
                           ),
                         ),
-                      ),
-                      EspacamentoH(h: 17),
-                      TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          labelText: 'Senha',
-                          prefixIcon:
-                              Icon(Icons.lock, color: Colors.blueGrey[800]),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
+                        EspacamentoH(h: 17),
+                        TextField(
+                          controller: _controller.senha_usuario,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: 'Senha',
+                            prefixIcon: Icon(Icons.lock, color: Colors.blueGrey[800]),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
                           ),
                         ),
-                      ),
-                      EspacamentoH(h: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          TextButton(
-                            onPressed: () {},
-                            child: const Text('Esqueci minha senha'),
-                          ),
-                          Row(
-                            children: [
-                              Text('Lembrar-me'),
-                              Checkbox(
-                                value: _rememberMe,
-                                onChanged: (bool? value) {
-                                  setState(() {
-                                    _rememberMe = value!;
-                                  });
-                                },
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      EspacamentoH(h: 15),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushReplacementNamed(
-                            context,
-                            '/painel',
-                          );
-                        },
-                        child: const Text('Entrar'),
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          backgroundColor: Color.fromRGBO(9, 68, 121, 1),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 15, horizontal: 30),
+                        EspacamentoH(h: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text('Esqueci minha senha'),
+                            ),
+                            Row(
+                              children: [
+                                Text('Lembrar-me'),
+                                Checkbox(
+                                  value: _rememberMe,
+                                  onChanged: (bool? value) {
+                                    setState(() {
+                                      _rememberMe = value!;
+                                    });
+                                  },
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
+                        EspacamentoH(h: 15),
+                        ElevatedButton(
+                          onPressed: _isLoading ? null : _tentarLogin, // Desativa o botão enquanto o login está sendo processado
+                          child: _isLoading
+                              ? CircularProgressIndicator(
+                                  color: Colors.white,
+                                )
+                              : const Text('Entrar'),
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Color.fromRGBO(9, 68, 121, 1),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 15, horizontal: 30),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
--- lib/view/perfil_view.dart
+++ lib/view/perfil_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_gerdau/controller/login_controller.dart';
 import 'package:login_gerdau/view/components/appbar_components.dart';
 import 'package:login_gerdau/view/components/espacamento_h.dart';
 
@@ -10,100 +11,128 @@ class PerfilView extends StatefulWidget {
 }
 
 class _PerfilViewState extends State<PerfilView> {
+  String? nomeUsuario;
+  String? emailUsuario;
+
+  
+  final LoginController _controller = LoginController();
+
+  @override
+  void initState() {
+    super.initState();
+    _carregarDadosPerfil();
+  }
+
+  // Método para carregar dados do perfil (nome e email)
+  Future<void> _carregarDadosPerfil() async {
+    // Recupera os dados armazenados ou da API
+    final nome = await _controller.obterNomeUsuario();
+    final email = await _controller.obterEmailUsuario();
+
+    setState(() {
+      nomeUsuario = nome;
+      emailUsuario = email;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       body: NestedScrollView(
-      headerSliverBuilder: (context, innerBoxScrolled) => [
-        AppbarfotoComponents(title: 'Perfil'),
-      ],
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Column(
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxScrolled) => [
+          AppbarfotoComponents(title: 'Perfil'),
+        ],
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      elevation: 5,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          children: [
+                            ClipOval(
+                              child: Image.asset(
+                                'assets/images/redondoPerfil.jpg',
+                                width: 120,
+                                height: 120,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            const EspacamentoH(h: 10),
+                            Text(
+                              nomeUsuario ?? 'Nome não disponível',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const EspacamentoH(h: 10),
+                            Text(
+                              emailUsuario ?? 'E-mail não disponível',
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Divider(height: 40, thickness: 1),
+              const EspacamentoH(h: 15),
+              const ListTile(
+                leading: Icon(Icons.work),
+                title: Text('Cargo'),
+                subtitle: Text('Encarregado Deposito'),
+              ),
+              const ListTile(
+                leading: Icon(Icons.business),
+                title: Text('Departamento'),
+                subtitle: Text('Administrativo'),
+              ),
+              const ListTile(
+                leading: Icon(Icons.location_on),
+                title: Text('Localização'),
+                subtitle: Text('Pindamonhangaba - SP'),
+              ),
+              const ListTile(
+                leading: Icon(Icons.badge),
+                title: Text('Chapa'),
+                subtitle: Text('023211-0'),
+              ),
+              const EspacamentoH(h: 23),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
+                  ElevatedButton(
+                    onPressed: _controller.logout, // Chama o método de logout
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: const Color.fromRGBO(121, 9, 9, 1),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 15, horizontal: 30),
                     ),
-                    elevation: 5,
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(children: [
-                        ClipOval(
-                          child: Image.asset(
-                            'assets/images/redondoPerfil.jpg',
-                            width: 120,
-                            height: 120,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        const EspacamentoH(h: 10),
-                        const Text(
-                          'Kayky da Silva Costa',
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
-                        ),
-                        const EspacamentoH(h: 10),
-                        const Text(
-                          'kayky7costa@hotmail.com',
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                      ]),
-                    ),
+                    child: const Text('Sair'),
                   ),
                 ],
               ),
-            ),
-            const Divider(height: 40, thickness: 1),
-            const EspacamentoH(h: 15),
-            const ListTile(
-              leading: Icon(Icons.work),
-              title: Text('Cargo'),
-              subtitle: Text('Encarregado Deposito'),
-            ),
-            const ListTile(
-              leading: Icon(Icons.business),
-              title: Text('Departamento'),
-              subtitle: Text('Administrativo'),
-            ),
-            const ListTile(
-              leading: Icon(Icons.location_on),
-              title: Text('Localização'),
-              subtitle: Text('Pindamonhangaba - SP'),
-            ),
-            const ListTile(
-              leading: Icon(Icons.badge),
-              title: Text('Chapa'),
-              subtitle: Text('023211-0'),
-            ),
-            const EspacamentoH(h: 23),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(context, '/login');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: const Color.fromRGBO(121, 9, 9, 1),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 15, horizontal: 30),
-                  ),
-                  child: const Text('Sair'),
-                ),
-              ],
-            )
-          ],
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
