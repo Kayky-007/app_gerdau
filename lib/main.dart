8246f67a9550aec379b16ac6e6150c04a563f68a
Guilherme Ribeiro <guilhermeguiribeiro22@gmail.com>
2024-11-14 15:08:33 -0300

    Verificação de Token

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,45 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dropdown_alert/dropdown_alert.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:login_gerdau/view/cardapio_view.dart';
-
 import 'package:login_gerdau/view/login_view.dart';
 import 'package:login_gerdau/view/painel_view.dart';
 import 'package:login_gerdau/view/pedidos_view.dart';
 import 'package:page_transition/page_transition.dart';
-
-void main() => runApp(const MyApp());
-
+ 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+ 
+  // Instanciando o FlutterSecureStorage
+  final storage = FlutterSecureStorage();
+ 
+  // Obtendo o token armazenado
+  String? token = await storage.read(key: 'token');
+ 
+  runApp(MyApp(token: token));
+}
+ 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  final String? token;
+ 
+  const MyApp({super.key, this.token});
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       builder: (context, child) => Stack(
         children: [child!, DropdownAlert()],
       ),
-      //cores do app
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
         appBarTheme: AppBarTheme(
-            foregroundColor: Colors.white,
-            backgroundColor: Color.fromRGBO(1, 52, 91, 1)),
+          foregroundColor: Colors.white,
+          backgroundColor: Color.fromRGBO(1, 52, 91, 1),
+        ),
         inputDecorationTheme: InputDecorationTheme(
           enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey), 
+            borderSide: BorderSide(color: Colors.grey),
             borderRadius: BorderRadius.circular(10.0),
           ),
           focusedBorder: OutlineInputBorder(
@@ -35,21 +47,23 @@ class MyApp extends StatelessWidget {
                 color: Color.fromRGBO(129, 108, 12, 1), width: 2.0),
             borderRadius: BorderRadius.circular(10.0),
           ),
-          labelStyle: TextStyle(color: Colors.grey), 
+          labelStyle: TextStyle(color: Colors.grey),
           floatingLabelStyle:
-              TextStyle(color: Color.fromRGBO(129, 108, 12, 1)), 
+              TextStyle(color: Color.fromRGBO(129, 108, 12, 1)),
         ),
       ),
       routes: {
-        // Splash Screen iniciando a aplicação e redirecionando para a tela de login
         '/': (context) => AnimatedSplashScreen(
-            duration: 2000,
-            splash: 'assets/images/logoGRSA_splashscreen.png',
-            nextScreen: LoginView(),
-            splashTransition: SplashTransition.fadeTransition,
-            pageTransitionType: PageTransitionType.topToBottom,
-            splashIconSize: 150,
-            backgroundColor: Colors.white),
+              duration: 2000,
+              splash: 'assets/images/logoGRSA_splashscreen.png',
+              nextScreen: token != null
+                  ? const PainelView() // Se o token estiver presente, vai para o painel
+                  : const LoginView(), // Caso contrário, vai para a tela de login
+              splashTransition: SplashTransition.fadeTransition,
+              pageTransitionType: PageTransitionType.topToBottom,
+              splashIconSize: 150,
+              backgroundColor: Colors.white,
+            ),
         '/painel': (context) => const PainelView(),
         '/login': (context) => const LoginView(),
         '/cardapio': (context) => const CardapioView(),
